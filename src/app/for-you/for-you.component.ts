5c720aefcfbc19ba69c13dc60e5345556b42c063
lenyDENOYELLE <lenydenoyelle@gmail.com>
2025-02-11 16:59:13 +0100

    ajout de for-you

---
+++ src/app/for-you/for-you.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-for-you',
+  standalone: true,
+  imports: [],
+  templateUrl: './for-you.component.html',
+  styleUrl: './for-you.component.sass'
+})
+export class ForYouComponent {
+
+}
