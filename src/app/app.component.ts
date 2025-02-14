1cb383ef5ff8da290c0385a083f373ed3cf9701f
lenyDENOYELLE <lenydenoyelle@gmail.com>
2025-02-14 18:06:48 +0100

    Ajout des composants nécessaires

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { ForYouComponent } from './for-you/for-you.component';
+import { NosServicesComponent } from './nos-services/nos-services.component';
+import { CustomersComponent } from './customers/customers.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +13,9 @@ import { ForYouComponent } from './for-you/for-you.component';
     RouterOutlet,
     HeaderComponent,
     ForYouComponent,
+    NosServicesComponent,
+    CustomersComponent,
+    FooterComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
---
+++ src/app/nos-services/grid-services/grid-services.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-services',
+  standalone: true,
+  imports: [],
+  templateUrl: './grid-services.component.html',
+  styleUrl: './grid-services.component.sass'
+})
+export class GridServicesComponent {
+
+}
