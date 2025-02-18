8d80279a4b4606e64b73afad195250eaf5d3ff5e
lenyDENOYELLE <lenydenoyelle@gmail.com>
2025-02-18 16:07:03 +0100

    Creation des routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { VoyagePageComponent } from './voyage-page/voyage-page.component';
+import { GenereVoyagePageComponent } from './genere-voyage-page/genere-voyage-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        component: HomePageComponent,
+        path: '',
+    },
+    {
+        component: VoyagePageComponent,
+        path: 'voyage',
+    },
+    {
+        component: GenereVoyagePageComponent,
+        path: 'generer',
+    },
+];
